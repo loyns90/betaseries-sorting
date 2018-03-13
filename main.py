bbd37af6d9d35ab8337bbbb102f9a73d607985bb
Loyns <riou.pro@gmail.com>
2018-03-13 20:15:28 +0100

    Recuperation des informations pour Eztv : OK

--- main.py
+++ main.py
@@ -43,7 +43,8 @@ eztvAPI = "https://eztv.ag/api/get-torrents?imdb_id="
 # Methodes RarBG
 # **************
 
-rarbgSearch = "https://rarbgproxy.org/torrents.php?search=black+mirror+s04e01"
+rarbgSearch = "https://rarbgproxy.org/torrents.php?search="
+rarbgParams = "&order=size&by=ASC"
 
 # **************************************************
 
@@ -51,7 +52,7 @@ rarbgSearch = "https://rarbgproxy.org/torrents.php?search=black+mirror+s04e01"
 # Methodes KAT
 # ************
 
-katSearch = "https://katcr.co/katsearch/page/1/black-mirror-s04e01"
+katSearch = "https://katcr.co/katsearch/page/1/"
 
 # **************************************************
 
@@ -69,24 +70,64 @@ getEpsToSeeListById= bsf.reqBS(getEpsToSeeList, False)
 #        print(ep["show"]["title"])
 #        print(ep["code"])
     #myEpsToSee.append(show["title"])
+epiToSearch="vikings s05e10"
+epStrMin = epiToSearch.replace(' ','-')
+epStrPl = epiToSearch.replace(' ','+')
+epStrPt = epiToSearch.replace(' ','.')
+getEztvLinks = os.popen("curl " + eztvSearch + epStrMin).read()
 
-getUrl = os.popen("curl https://katcr.co/katsearch/page/1/black-mirror-s04e01 | grep magnet").read()
-soup = BeautifulSoup(getUrl, "html.parser")
+# Validé :
+# ********
+# getKatLinks = os.popen("curl " + katSearch + epStrMin + " | grep magnet").read()
+# getEztvLinks = os.popen("curl " + eztvSearch + epStrMin).read()
+# ********
+
+# À valider :
+# ***********
+# getRarBGLinks = os.popen("curl " + rarbgSearch  + epStrPl).read()
+# ***********
+
+soup = BeautifulSoup(getEztvLinks, "html.parser")
 balises_links = soup.find_all('a')
 balises_tds = soup.find_all('td')
 links = []
 sizes = []
 links_list = []
 
+# Traitement Kat
+# **************
+#for td in balises_tds:
+#  if 'Size' in str(td):
+#    sizes.append(str(td.string))
+#for link in balises_links:
+#  if 'magnet' in link.get('href'):
+#    links.append(link.get('href'))
+#for i in range(len(links)):
+#  entry = {'magnet': links[i], 'size': sizes[i]}
+#  links_list.append(entry)
+
+# Traitement RarBG
+# ****************
+#for link in balises_links:
+#  if 'torrent' in link.get('href'):
+#    links.append(link.get('href'))
+#    print(link.get('href'))
+#for i in range(len(links)):
+#  entry = {'magnet': links[i], 'size': sizes[i]}
+#  links_list.append(entry)
+
+# Traitement EZTV
+# **************
 for td in balises_tds:
-  if 'Size' in str(td):
+  if 'MB' in str(td.string) or 'GB' in str(td.string):
     sizes.append(str(td.string))
 for link in balises_links:
-  if 'magnet' in link.get('href'):
+  if 'magnet' in link.get('href') and epStrPt.lower() in link.get('href').lower():
     links.append(link.get('href'))
 for i in range(len(links)):
   entry = {'magnet': links[i], 'size': sizes[i]}
   links_list.append(entry)
+
 
 for objLink in links_list:
   print(objLink['magnet'] + " - " + objLink['size'])
